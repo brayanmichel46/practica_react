e8af6955e98bff9fd29a1d677a0005ca2d5ec5b4
Brayan Michel Villota Quintero <bvillota@bancolombia.com.co>
2022-06-26 18:50:53 -0500

    [ADD] GitHub Actions

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext'
+import '../styles/OrderItem.scss';
+
+const OrderItem = ({ product }) => {
+	const { removeFromCart } = useContext(AppContext);
+	const handleRemove = product => {
+		removeFromCart(product);
+	}
+	return (
+		<div className="OrderItem">
+			<figure>
+				<img src={product.images[0]} alt="bike" />
+			</figure>
+			<p>{product.title}</p>
+			<p>{product.price}</p>
+			<img src="./icons/icon_close.png" alt="close" onClick={() => handleRemove(product)} />
+		</div>
+	);
+}
+
+export default OrderItem;
